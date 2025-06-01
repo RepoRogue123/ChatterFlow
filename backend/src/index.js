222bfe6d61bd975413030cd3b16c0965377c6043
RepoRogue123 <b23ee1006@iitj.ac.in>
2025-06-02 00:29:47 +0530

    Integrated frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",                     
+      "https://chatter-flow-one.vercel.app/login",          
+    ],
     credentials: true,
   })
 );
